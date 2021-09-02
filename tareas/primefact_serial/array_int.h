90b238d888dd9cc2737e102d2f248dfd1cbaf9f8
gustavopintocr <gustavo.pinto@ucr.ac.cr>
2021-09-02 17:35:12 -0600

    Mejoras e implementacion de mas metodos en los arrays

---
+++ tareas/primefact_serial/array_int.h
@@ -0,0 +1,35 @@
+/*
+ * Ⓒ 2021 Gustavo Pinto Some Rights Reserved
+ */
+
+#ifndef ARRAY_INT_H
+#define ARRAY_INT_H
+#include <stddef.h>
+#include <stdint.h>
+
+typedef struct sub_array{
+  size_t capacity;
+  size_t counter;
+  int64_t number;
+  int64_t* bases;
+  int64_t* exponents;
+}sub_array_t;
+
+typedef struct {
+  size_t capacity;
+  size_t counter;
+  sub_array_t* elements;
+} array_int_t;
+
+int add_element(array_int_t* array, int64_t number);
+int add_subarray(sub_array_t* a, int64_t base, int64_t expo);
+void destroy_array(array_int_t* array);
+void destroy_subarray(array_int_t* array);
+void factorize(array_int_t* array);
+int increase_capacity(array_int_t* array);
+int increase_subarray(sub_array_t* array);
+void start_array(array_int_t* array);
+void start_subarray(sub_array_t* array);
+void print(array_int_t* array);
+
+#endif  // ARRAY_INT_Hmentos[i].numero);
